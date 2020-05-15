11c2883f66fc901d13d017b7d8483e6e9e1e1467
gorskii <egor.yakayaka@gmail.com>
2020-05-16 01:41:56 +0400

    Implement await writing in handler function

--- w5_asyncio_tcp_client.py
+++ w5_asyncio_tcp_client.py
@@ -4,11 +4,14 @@
 import asyncio
 
 
-async def tcp_echo_client(message, loop):
-    pass
+async def tcp_echo_client(message):
+    reader, writer = await asyncio.open_connection("127.0.0.1", 10001)
+    print(f"send: {message}")
+    writer.write(message.encode())
+    writer.close()
 
 
 loop = asyncio.get_event_loop()
 message = "Hello, World!"
-loop.run_until_complete(tcp_echo_client(message, loop))
+loop.run_until_complete(tcp_echo_client(message))
 loop.close()
