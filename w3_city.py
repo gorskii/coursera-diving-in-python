12c367c5e73a9648fa7fbcce1227afe6e62ac9dd
gorskii <egor.yakayaka@gmail.com>
2020-05-18 03:42:04 +0400

    Refactor w3_city.py and dump forecast as json
    Replace some loops with dict and list comprehensions
    Add type annotations to functions using typing module
    Remove pprint module
    Use json module to write formatted data to file

--- w3_city.py
+++ w3_city.py
@@ -1,58 +1,62 @@
 """Simple forecast module which uses OpenWeatherMap API
 """
 
-import pprint
+from typing import List, Dict
+import json
 import requests
 from dateutil.parser import parse
 
 
 class WeatherForecast:
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._city_cache = {}
 
-    def get(self, city):
+    def get_forecast(self, city: str) -> List[Dict[str, str]]:
         if city in self._city_cache:
+            print(f"Getting '{city}' forecast from cache")
             return self._city_cache[city]
         url = f"https://api.openweathermap.org/data/2.5/forecast?" \
               f"q={city}&units=metric&appid=967e0644610deec378355a38548a57ee"
         print("Sending HTTP request")
         data = requests.get(url).json()
         forecast_data = data["list"]
-        forecast = []
-        for daytime_data in forecast_data:
-            forecast.append({
-                "datetime": parse(daytime_data["dt_txt"]),
-                "high_temp": daytime_data["main"]["temp_max"]
-            })
+        forecast = [
+            {
+                "datetime": str(parse(daytime_data["dt_txt"])),
+                "temp_max": str(daytime_data["main"]["temp_max"])
+            }
+            for daytime_data in forecast_data
+        ]
         self._city_cache[city] = forecast
         return forecast
 
 
 class CityInfo:
 
-    def __init__(self, city: str, weather_forecast=None):
+    def __init__(self,
+                 city: str,
+                 weather_forecast=WeatherForecast()) -> None:
         self.city = city
-        self._weather_forecast = weather_forecast or WeatherForecast()
+        self._weather_forecast = weather_forecast
 
-    def weather_forecast(self):
-        return self._weather_forecast.get(self.city)
+    def weather_forecast(self) -> List[Dict[str, str]]:
+        return self._weather_forecast.get_forecast(self.city)
 
 
-def _main():
-    forecast = {}
-    weather_forecast = WeatherForecast()
-    for city in ["Moscow", "Saratov", "Kazan"]:
-        for _ in range(5):
-            city_info = CityInfo(city, weather_forecast=weather_forecast)
-            forecast[city] = city_info.weather_forecast()
+def _main() -> None:
+    cities = ["Moscow", "Saratov", "Kazan"]
+    forecaster = WeatherForecast()
+    forecast = {
+        city: CityInfo(city, weather_forecast=forecaster).weather_forecast()
+        for city in cities
+    }
 
     with open("forecast.txt", "w") as f:
-        for key, value in forecast.items():
-            f.write(str(key) + ": " + str(value))  # TODO fix file output format
+        f.write(json.dumps(forecast, indent=2))
 
     with open("forecast.txt", "r") as f:
-        pprint.pprint(f.read())
+        print(f.read())
 
 
 if __name__ == "__main__":
