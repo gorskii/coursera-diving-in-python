6431f3847fa7dbc1a7ff88b6f673657f19546675
gorskii <egor.yakayaka@gmail.com>
2020-06-06 23:08:33 +0400

    Add socket attribute and create socket connection

--- w5/w5_metrics_client.py
+++ w5/w5_metrics_client.py
@@ -2,6 +2,7 @@
     Allows to send and receive CPU, memory, disk and network usage metrics
     using 'put' and 'get' commands respectively.
 """
+import socket
 from typing import Dict, List, Tuple
 
 
@@ -10,12 +11,19 @@ class Client:
         self._host = host
         self._port = port
         self._timeout = None or timeout
+        self._socket = self._connect()
 
     def get(self, metric: str) -> Dict[str, List[Tuple[int, float]]]:
         pass
 
     def put(self, metric: str, value: float, timestamp: int = None) -> None:
         pass
+
+    def _connect(self) -> socket.socket:
+        return socket.create_connection(
+            (self._host, self._port),
+            self._timeout
+        )
 
 
 class ClientError(BaseException):
