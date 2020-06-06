ed842c75ea21c83fd7f00971a6bf3cbbfcc96ea7
gorskii <egor.yakayaka@gmail.com>
2020-06-06 17:57:50 +0400

    Basic Client class

---
+++ w5/w5_metrics_client.py
@@ -0,0 +1,18 @@
+""" A client to communicate with metrics storage server using TCP-sockets.
+    Allows to send and receive CPU, memory, disk and network usage metrics
+    using 'put' and 'get' commands respectively.
+"""
+from typing import Dict, List, Tuple
+
+
+class Client:
+    def __init__(self, host: str, port: int, timeout: int = None) -> None:
+        self._host = host
+        self._port = port
+        self._timeout = None or timeout
+
+    def get(self, metric: str) -> Dict[str: List[Tuple[int, float]]]:
+        pass
+
+    def put(self, metric: str, value: float, timestamp: int) -> None:
+        pass
