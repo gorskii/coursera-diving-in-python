6d5f38aa8f4dc54005b429d34a42970e14d1f4f7
gorskii <egor.yakayaka@gmail.com>
2020-06-06 23:22:36 +0400

    Tests: connection, get metrics and put metrics with and without timestamp

--- tests/test_w5_metrics_client.py
+++ tests/test_w5_metrics_client.py
@@ -1,9 +1,60 @@
 from unittest import TestCase
 
+from w5.w5_metrics_client import Client
+
 
 class TestClient(TestCase):
-    def test_get(self):
-        self.fail()
+    def setUp(self) -> None:
+        self.host = "127.0.0.1"
+        self.port = 8888
 
-    def test_put(self):
-        self.fail()
+    def test_create_connection(self):
+        from socket import socket
+        client = Client(self.host, self.port)
+        self.assertIsInstance(client._socket, socket)
+
+    def test_get_all_metrics(self):
+        client = Client(self.host, self.port)
+        expected = {
+            'palm.cpu': [
+                (1150864247, 0.5),
+                (1150864248, 0.5)
+            ],
+            'eardrum.cpu': [
+                (1150864250, 3.0),
+                (1150864251, 4.0)
+            ],
+            'eardrum.memory': [
+                (1503320872, 4200000.0)
+            ]
+        }
+        self.assertEqual(expected, client.get("*"))
+
+    def test_get_one_metric(self):
+        client = Client(self.host, self.port)
+        expected = {
+            'palm.cpu': [
+                (1150864247, 0.5),
+                (1150864248, 0.5)
+            ]
+        }
+        self.assertEqual(expected, client.get("palm.cpu"))
+
+    def test_put_single_metric_with_timestamp(self):
+        client = Client(self.host, self.port)
+        metric_key = "palm.cpu"
+        metric_value = 0.5
+        metric_timestamp = 1150864247
+        client.put(metric_key, metric_value, metric_timestamp)
+        expected = (metric_timestamp, metric_value)
+        self.assertIn(expected, client.get(metric_key)[metric_key])
+
+    def test_put_single_metric_without_timestamp(self):
+        import time
+        client = Client(self.host, self.port)
+        metric_key = "palm.cpu"
+        metric_value = 0.5
+        metric_timestamp = time.time()
+        client.put(metric_key, metric_value)
+        expected = (metric_timestamp, metric_value)
+        self.assertIn(expected, client.get(metric_key)[metric_key])
